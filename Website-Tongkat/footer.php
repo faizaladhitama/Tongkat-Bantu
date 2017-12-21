bffef12e3d58aa4fe89ba21cac47ea1bcffa9eac
faizaladhitama <faizaladhitamaprabowo@gmail.com>
2017-12-21 11:26:11 +0700

    Add footer,header,index

---
+++ Website-Tongkat/footer.php
@@ -0,0 +1,9 @@
+	<footer class="container-fluid text-center">
+		<p>Copyright © 2017 Seorang Pahlawan</p>
+		<p>Faizal Adhitama Prabowo</p>
+		<p>Gibran Muhammad Fajar Wisesa</p>
+		<p>Muhammad Faiz</p>
+		<p>Muhammad Farhan Emir</p>
+	</footer>
+</body>
+</html>
